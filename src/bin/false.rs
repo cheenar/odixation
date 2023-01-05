8417059b350400169a1abea0cd760e00d94cbd13
cheenar@ <cheenar@hey.com>
2023-01-05 02:17:20 -0500

    [Basic] Implement True/False
    Changes:
    
    * Adds true.rs, false.rs implementations
    * Uses process:exit to achieve goal
    
    Testing:
    
    * cargo run succeeded
    * cargo run --bin true/false
    
    Issue: https://github.com/cheenar/odixation/issues/2

---
+++ src/bin/false.rs
@@ -0,0 +1,8 @@
+use std::process::exit;
+
+// success denoted by non-zero return
+const DEFAULT_ERROR_CODE: i32 = 1;
+
+fn main() {
+    exit(DEFAULT_ERROR_CODE);
+}
---
+++ src/bin/true.rs
@@ -0,0 +1,8 @@
+use std::process::exit;
+
+// success denoted by zero return
+const DEFAULT_ERROR_CODE: i32 = 0;
+
+fn main() {
+    exit(DEFAULT_ERROR_CODE);
+}
